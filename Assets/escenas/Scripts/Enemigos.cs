da973593b014a92dd09863c93b740d045a98e160
manu138 <manu138@hotmail.es>
2016-08-31 17:21:46 -0500

    enemyspawner y enemy

---
+++ Assets/escenas/Scripts/Enemigos.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemigos : MonoBehaviour
+{
+    public GameObject enemy;
+    public float maxPos = 4.33f;
+    public float minPos;
+    float timer;
+    public float delayTimer = 0f;
+    public float velocity = 1f;
+
+    // Use this for initialization
+    void Start()
+    {
+        timer = delayTimer;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer = timer - Time.deltaTime;
+        if (timer <= 0)
+        {
+            Vector3 enemyPos = new Vector3(transform.position.x, Random.Range(-3.38f, 4.68f), transform.position.z);
+
+            Instantiate(enemy, enemyPos, transform.rotation);
+            timer = delayTimer;
+          
+        }
+      
+
+    }
+}
---
+++ Assets/escenas/Scripts/EnemyMov.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMov : MonoBehaviour {
+    public GameObject enemy;
+    float timeMissile = 0.5f;
+    public GameObject missile;
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        enemy.transform.Translate(new Vector3( -3*Time.deltaTime,0, 0));
+
+
+        if (timeMissile < 0)
+        {
+            var newMissile = (GameObject)Instantiate(missile, transform.position, transform.rotation);
+
+            newMissile.GetComponent<Rigidbody2D>().AddForce(new Vector2(-400, 0));
+            timeMissile = 0.5f;
+          
+        }
+        else
+        {
+            timeMissile -= Time.deltaTime;
+        }
+    }
+}
