153be32ae0506e0a5bb171bcaafce55d679f7d24
Juan Fernando Moreno Rojas <juan.morenor@upb.edu.co>
2016-08-31 15:42:03 -0500

    movimiento nave

---
+++ Assets/scripts/movimiento.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movimiento : MonoBehaviour {
+
+    public int force;
+
+
+    public Rigidbody2D rb;
+
+    // Use this for initialization
+    void Start () {
+
+        force = 10;
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey("up"))
+        {
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0, force));
+           
+}
+    
+         if (Input.GetKey("down"))
+        {
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0, -1*force));
+
+        }
+
+        if (Input.GetKey("left"))
+        {
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(-1 * force, 0));
+
+        }
+
+        if (Input.GetKey("right"))
+        {
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(force, 0));
+
+        }
+
+    }
+
+}
---
+++ Assets/scripts/shoot.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class shoot : MonoBehaviour {
+
+    public GameObject bulletPrefab;
+
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey("left"))
+        {
+
+            var bullet = (GameObject)Instantiate(bulletPrefab);
+        }
+}
+
+
+}
